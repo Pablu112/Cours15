3b05b6cf9e6f95710f1a677793ba9c6ab6586a1b
Pablu112 <yoohan438@gmail.com>
2025-12-12 16:00:48 +0500

    updated to have player object and platform with jump physics.

--- www/code/main.js
+++ www/code/main.js
@@ -1,24 +1,29 @@
-kaplay();
-loadBean();
-setGravity(1600);
-
-const player = add([
-	sprite("bean"),
-	pos(center()),
-	area(),
-	body(),
-]);
-
-add([
-	rect(width(), 48),
-	outline(4),
-	area(),
-	pos(0, height() - 48),
-	body({ isStatic: true }),
-]);
-
-onClick(() => {
-	if (player.isGrounded()) {
-		player.jump();
-	}
+const k = kaplay({
+	width: 855,
+	height: 415
 });
+
+k.loadBean();
+
+k.setGravity(1000);
+
+const player = k.add([
+	k.sprite("bean"),
+	k.pos(k.center()),
+	k.area(),
+	k.body(),
+])
+
+onClick(
+	() => {
+		if (player.isGrounded()) player.jump()
+	})
+
+
+k.add([
+	k.rect(k.width(), 120),
+	k.pos(0, 325),
+	k.area(),
+	k.outline(4),
+	k.body({ isStatic: true }),
+])
